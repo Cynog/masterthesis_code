0b62d5185c4ba499388209a61ec8e6a871b38137
Cynog <thomasratzketr@outlook.de>
2025-02-24 22:27:56 +0100

    fixed small typo in cluster run script

--- src/wc/run.sh
+++ src/wc/run.sh
@@ -2,7 +2,7 @@
 
 
 # create folder for slurm logs
-mkdir -p ../../_logs/dw
+mkdir -p ../../_logs/wc
 
 # run the snakemake pipeline on slurm cluster
 snakemake --profile _profile --config dimension=$1 -r ${@:2}
